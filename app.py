3b8b392776dc2f058765a56e02bd17bf5951046b
Umang-Rajkarnikar <59628796+Umang-Rajkarnikar@users.noreply.github.com>
2020-11-21 20:11:40 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-clf_model = pickle.load('model.pkl', 'rb')
+clf_model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def home():
@@ -15,10 +15,10 @@ if __name__ == "__main__":
 @app.route('/submit', methods=['POST'])
 def run():
         a = []
-        for i in range(3):
+        for i in range(6):
             a[i] = request.form["attribute" + str(i)]
-            #f(a[i] == null):
-            #    a[i] = 0
+            if(a[i] == null && i < 5):
+                a[i] = 0
         arr = np.array(a)
         pred = clf_model.predict(arr)
         return render_template("index.html", data=pred)
